ad1c218f6627a0346a93aa7f5b5e7d4c5a0d47ea
rituraj-pritish <riturajpritish@gmail.com>
2020-05-16 11:42:25 +0530

    removed 3rd redux-firestore

--- src/index.js
+++ src/index.js
@@ -1,57 +1,41 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { Provider } from 'react-redux';
-import { applyMiddleware, createStore } from 'redux';
-import rootReducer from './redux/reducers';
-import { composeWithDevTools } from 'redux-devtools-extension';
-import reduxThunk from 'redux-thunk';
-import { createFirestoreInstance } from 'redux-firestore';
-import { ReactReduxFirebaseProvider } from 'react-redux-firebase';
-import { persistStore, persistReducer } from 'redux-persist';
-import { PersistGate } from 'redux-persist/integration/react';
-import storage from 'redux-persist/lib/storage';
-import firebase from './firebase/firebase';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
+import rootReducer from './redux/reducers'
+import { composeWithDevTools } from 'redux-devtools-extension'
+import reduxThunk from 'redux-thunk'
+import { persistStore, persistReducer } from 'redux-persist'
+import { PersistGate } from 'redux-persist/integration/react'
+import storage from 'redux-persist/lib/storage'
 
-import './index.css';
-import App from './components/App';
-import Loader from './components/layout/loader/Loader';
+import './index.css'
+import App from './components/App'
+import Loader from './components/layout/loader/Loader'
 
 const persistConfig = {
   key: 'root',
   storage
-};
+}
 
-const persistedReducer = persistReducer(persistConfig, rootReducer);
+const persistedReducer = persistReducer(persistConfig, rootReducer)
 
-const rrfConfig = {
-  Profile: 'users',
-  useFirestoreForProfile: true
-};
-
-const middlewares = [reduxThunk];
+const middlewares = [reduxThunk]
 
 const devTools =
   process.env.NODE_ENV === 'production'
     ? applyMiddleware(...middlewares)
-    : composeWithDevTools(applyMiddleware(...middlewares));
-const store = createStore(persistedReducer, devTools);
+    : composeWithDevTools(applyMiddleware(...middlewares))
 
-const persistor = persistStore(store);
+const store = createStore(persistedReducer, devTools)
 
-const rrfProps = {
-  firebase,
-  config: rrfConfig,
-  dispatch: store.dispatch,
-  createFirestoreInstance
-};
+const persistor = persistStore(store)
 
 ReactDOM.render(
   <Provider store={store}>
     <PersistGate loading={<Loader />} persistor={persistor}>
-      <ReactReduxFirebaseProvider {...rrfProps}>
-        <App />
-      </ReactReduxFirebaseProvider>
+      <App />
     </PersistGate>
   </Provider>,
   document.getElementById('root')
-);
+)
