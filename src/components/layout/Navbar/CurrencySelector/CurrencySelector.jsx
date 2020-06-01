14e84ae4e31de68a48a248d6295dcbd858d42354
rituraj-pritish <pritishrituraj@gmail.com>
2020-06-01 20:25:41 +0530

    added multiple currencies

--- src/components/layout/Navbar/CurrencySelector/CurrencySelector.jsx
+++ src/components/layout/Navbar/CurrencySelector/CurrencySelector.jsx
@@ -1,17 +1,23 @@
 import React from 'react'
+import currencyList from 'redux/currency/currencyList'
 
 const CurrencySelector = ({ changeCurrency, currencyCode }) => {
   const handleChange = e => {
     changeCurrency(e.target.value)
   }
+
   return (
     <form>
       <label for='currency'>Currency</label>
       <select name='currency' id='currency' onChange={handleChange}>
-        <option selected={currencyCode === 'USD'} value='USD'>USD</option>
-        <option selected={currencyCode === 'INR'} value='INR'>INR</option>
-        {/* <option value='opel'>Opel</option>
-        <option value='audi'>Audi</option> */}
+        {/* <option selected={currencyCode === 'USD'} value='USD'>USD</option>
+        <option selected={currencyCode === 'INR'} value='INR'>INR</option> */}
+        {Object.values(currencyList).map(({ code, name_plural }) => (
+          <option
+            selected={currencyCode === code}
+            value={code}
+          >{`${code} - ${name_plural}`}</option>
+        ))}
       </select>
     </form>
   )
